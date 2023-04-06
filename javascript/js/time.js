32a4bdd69caa1a26b20a645e938539754423cb28
Chirag301987 <Chirag.301987@gmail.com>
2023-04-07 02:06:29 +0530

    h

---
+++ javascript/js/time.js
@@ -0,0 +1,32 @@
+let d = new Date();
+let hour = d.getHours();
+let h = hour*3600
+let minutes = d.getMinutes();
+let m = minutes*60
+let seconds = d.getSeconds();
+let s = seconds
+let time = h+m+s;
+document.getElementById("demo").innerHTML = time;
+if (time<43199 && time>=21600) {
+    document.getElementById("hi").innerHTML = "Hello Good Morning"
+}else if (time=>43200 && time<=61199) {
+    document.getElementById("hi").innerHTML = "Hello Good Evening"
+}else if (time=>61200 && time<=72000) {
+    document.getElementById("hi").innerHTML = "Hello Good Afternoon"
+}else {
+    document.getElementById("hi").innerHTML = "Hello Good Night"
+}
+
+// if (time<43199 && time>=21600) {
+//     document.getElementById("hi").innerHTML = "Hello Good Morning"
+// } else {
+//     if (time=>43200 && time<=61199) {
+//         document.getElementById("hi").innerHTML = "Hello Good Evening"
+//     }  else {
+//         if (time=>61200 && time<=72000) {
+//             document.getElementById("hi").innerHTML = "Hello Good Afternoon"
+//         } else {
+//             document.getElementById("hi").innerHTML = "Hello Good Night"
+//         }
+//     }
+// }
