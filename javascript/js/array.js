6e13a66303f06db3116dfc28ceeffbed5fa7fa23
amharsh06 <kathrotiyaharsh8@gmail.com>
2023-04-19 15:28:59 +0530

    class

---
+++ javascript/js/array.js
@@ -0,0 +1,58 @@
+let rollNo = [15,28,7,36,77];
+
+// for (let i=0; i>rollNo.length; i++) {
+//     console.log(rollNo[i]);
+// }
+
+// rollNo.forEach (function (v, i) {
+//     console.log(v);
+// });
+
+// rollNo.forEach ((v,i) => {
+//     console.log(v);
+// });
+
+// rollNo.map ((v,i) =>{
+//     console.log(i ,v);
+    
+// });
+
+
+// chage array Element
+// rollNo[2] =90
+
+// rollNo.map ((v,i)=> console.log(i,v,));
+
+
+// add array Element
+// rollNo.map ((v,i) =>{
+//     if (i===3) {
+//         console.log(v+10);
+//     }else {
+//         console.log(v);
+//     }
+// })
+
+
+// Array are a special type of object.
+let data = ['chirag',35, 'surat',301];
+let data1 = ['laxminarayan',08]
+
+// data.push(99);
+// data.pop(data);
+
+// data.shift(data);
+// data.unshift('vaddoriya');
+
+// let d = data.concat(data1)
+
+// s = data.some ((v) => v>99)
+
+// console.log(s);
+
+// f =data.find ((v) => v>99)
+// console.log(f);
+
+data.splice(1,3, "Lemon", "Kiwi");
+
+console.log(data);
