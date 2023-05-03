cc3b1d7afcc8721870d4407876f406ec0c59763f
Chirag301987 <Chirag.301987@gmail.com>
2023-05-04 01:07:52 +0530

    home

--- javascript/js/budget.js
+++ javascript/js/budget.js
@@ -1,16 +1,17 @@
 let fd = document.getElementById("fdata");
 let exdata = document.getElementById("dataex");
+
 // let budgett = 0;
 // let expenses = 0;
 // let balance = budgett - expenses
-let arr =[]
+
 const handalsubmit = () => {
 
     let budgett = document.getElementById("bdata").value
     let aElem = document.createElement("p");
     let texElem = document.createTextNode(budgett);
     aElem.appendChild(texElem);
-    let divRef = document.getElementById("bu")
+    let divRef = document.getElementById("amount")
     divRef.appendChild(aElem);
 
     event.preventDefault();
@@ -21,11 +22,11 @@ const handalsubmit = () => {
 const handalsub = () => {
     let budgett = document.getElementById("bdata").value
     let expenses = document.getElementById("exda").value
-    console.log("1111");
+
     let exElem = document.createElement("p");
     let extexElem = document.createTextNode(expenses);
     exElem.appendChild(extexElem);
-    let exdivRef = document.getElementById("exp")
+    let exdivRef = document.getElementById("expenditure-value")
     exdivRef.appendChild(exElem);
 
     balance = budgett - expenses
@@ -33,22 +34,18 @@ const handalsub = () => {
     let baElem = document.createElement("p");
     let batexElem = document.createTextNode(balance);
     baElem.appendChild(batexElem);
-    let badivRef = document.getElementById("bal")
+    let badivRef = document.getElementById("balance-amount")
     badivRef.appendChild(baElem);
 
-    // let data = document.getElementById("exname").value;
-    // arr.push(data);
-    // document.getElementById('exname').value = "";
-   
+    let data = document.getElementById("exname").value;
+    arr.push(data);
+    document.getElementById('exname').value = "";
+
     let data1 = document.getElementById("exda").value;
     arr.push(data1);
 
-    let toElem = document.createElement("p");
-    let totexElem = document.createTextNode(data1);
-    toElem.appendChild(totexElem);
-    let todivRef = document.getElementById("doto1")
-    todivRef.appendChild(toElem);
-   
+
+
 
     event.preventDefault()
 }
